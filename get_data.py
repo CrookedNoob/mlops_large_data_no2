54f51fb794b609f574ce317580f8c22ff9da9fbb
Majumder <soumyadip.majumder@here.com>
2021-11-18 22:12:18 +0530

    generate data

---
+++ get_data.py
@@ -0,0 +1,14 @@
+from sklearn.datasets import make_regression
+import pandas as pd
+import os
+import numy as np
+
+if os.path.isfile('large_data.csv'):
+    n=1
+else:
+    n=50
+
+for i in range(0, n):
+    X, y= make_regression(100000, n_features=10)
+    df= pd.DataFrame(X)
+    df.to_csv('large_data.csv', mode='a')
